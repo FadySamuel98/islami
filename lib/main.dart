923b56f0530569a6fb0d1757dfd98c38de9b8447
fady <fadysamuel18@gmail.com>
2024-12-30 22:16:36 +0200

    create islami project create empty homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:HomeScreen() ,
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+      },
     );
   }
 }
